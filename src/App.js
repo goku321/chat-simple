3c4fd5cf958d094b7fc0913ed8ad2b1624cc9e95
deepak-sah <sah.sslpu@gmail.com>
2019-06-10 22:39:44 +0530

    Updated reducers

--- src/App.js
+++ src/App.js
@@ -1,40 +1,23 @@
 import React from 'react';
-
-function createStore(reducer, initialState) {
-  let state = initialState;
-  const listeners = [];
-
-  const subscribe = (listener) => (
-    listeners.push(listener)
-  );
-
-  const getState = () => (state);
-
-  const dispatch = (action) => {
-    state = reducer(state, action);
-    listeners.forEach(l => l());
-  };
-
-  return {
-    subscribe,
-    getState,
-    dispatch,
-  };
-}
+import { createStore } from 'redux';
+import uuid from 'uuid';
 
 function reducer(state, action) {
   if(action.type === 'ADD_MESSAGE') {
+    const newMessage = {
+      text: action.text,
+      timestamp: Date.now(),
+      id: uuid.v4(),
+    };
+
     return {
-      messages: state.messages.concat(action.message),
+      messages: state.messages.concat(newMessage),
     };
   } else if(action.type === 'DELETE_MESSAGE') {
     return {
-      messages: [
-        ...state.messages.splice(0, action.index),
-        ...state.messages.splice(
-          action.index+1, state.messages.length
-        ),
-      ],
+      messages: state.messages.filter(message => (
+        message.id !== action.id
+      )),
     };
   } else {
     return state;
@@ -51,5 +34,81 @@ class App extends React.Component {
 
   render() {
     const messages = store.getState().messages;
+
+    return (
+      <div className="ui segment">
+        <MessageView messages={messages} />
+        <MessageInput />
+      </div>
+    );
   }
 }
+
+class MessageView extends React.Component {
+  handleClick = (index) => {
+    store.dispatch({
+      type: 'DELETE_MESSAGE',
+      index: index,
+    });
+  }
+  render() {
+    const messages = this.props.messages.map((message, index) => (
+      <div
+        className='comment'
+        key={index}
+        onClick={() => this.handleClick(index)}
+      >
+        {message}
+      </div>
+    ));
+    return (
+      <div className="ui comments">
+        {messages}
+      </div>
+    );
+  }
+}
+
+class MessageInput extends React.Component {
+  state = {
+    value: '',
+  };
+
+  onChange = (e) => {
+    this.setState({
+      value: e.target.value,
+    });
+  }
+
+  handleSubmit = () => {
+    store.dispatch({
+      type: 'ADD_MESSAGE',
+      message: this.state.value,
+    });
+
+    this.setState({
+      value: '',
+    });
+  }
+
+  render() {
+    return (
+      <div className="ui input">
+        <input
+          onChange={this.onChange}
+          value={this.state.value}
+          type='text'
+        />
+        <button
+          onClick={this.handleSubmit}
+          className="ui primary button"
+          type='submit'
+        >
+          Submit
+        </button>
+      </div>
+    );
+  }
+}
+
+export default App;
