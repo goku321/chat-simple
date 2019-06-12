99a43dadca230814c95eb33442dcc77979207e16
deepak-sah <sah.sslpu@gmail.com>
2019-06-12 17:00:15 +0530

    Introduced Thread component

--- src/App.js
+++ src/App.js
@@ -24,7 +24,27 @@ function reducer(state, action) {
   }
 }
 
-const initialState = { messages: [] };
+const initialState = {
+  activeThreadId: '1-fac2',
+  threads: [
+    {
+      id: '1-fac2',
+      title: 'Ramesh',
+      messages: [
+        {
+          text: 'How are you, Ramesh?',
+          timestamp: Date.now(),
+          id: uuid.v4(),
+        },
+      ],
+    },
+    {
+      id: '2-be91',
+      title: 'Powar',
+      messages: [],
+    },
+  ],
+};
 const store = createStore(reducer, initialState);
 
 class App extends React.Component {
@@ -33,26 +53,28 @@ class App extends React.Component {
   }
 
   render() {
-    const messages = store.getState().messages;
+    const state = store.getState();
+    const activeThreadId = state.activeThreadId;
+    const threads = state.threads;
+    const activeThread = threads.find((t) => t.id === activeThreadId);
 
     return (
       <div className="ui segment">
-        <MessageView messages={messages} />
-        <MessageInput />
+        <Thread thread={activeThread} />
       </div>
     );
   }
 }
 
-class MessageView extends React.Component {
-  handleClick = (index) => {
+class Thread extends React.Component {
+  handleClick = (id) => {
     store.dispatch({
       type: 'DELETE_MESSAGE',
       id: id,
     });
   }
   render() {
-    const messages = this.props.messages.map((message) => (
+    const messages = this.props.thread.messages.map((message, index) => (
       <div
         className='comment'
         key={index}
@@ -65,8 +87,11 @@ class MessageView extends React.Component {
       </div>
     ));
     return (
-      <div className="ui comments">
-        {messages}
+      <div className='ui center aligned basic segment'>
+        <div className='ui comments'>
+          {messages}
+        </div>
+        <MessageInput />
       </div>
     );
   }
