7bdfd0b2e742f1f42617f18427ee3445fe2bb825
deepak-sah <sah.sslpu@gmail.com>
2019-06-20 13:33:09 +0530

    Using connect() to generate ThreadTabs

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Provider, connect } from 'react-redux';
 import { createStore, combineReducers } from 'redux';
 import uuid from 'uuid';
 
@@ -113,35 +114,35 @@ const Tabs = (props) => {
   );
 }
 
-class ThreadTabs extends React.Component {
-  componentDidMount = () => {
-    store.subscribe(() => this.forceUpdate());
-  }
+const mapStateToTabsProps = (state) => {
+  const tabs = state.threads.map(t => (
+    {
+      title: t.title,
+      active: t.id === state.activeThreadId,
+      id: t.id,
+    }
+  ));
 
-  handleClick = (id) => {
-    store.dispatch({
-      type: 'OPEN_THREAD',
-      id: id,
-    });
-  }
-  render() {
-    const state = store.getState();
-
-    const tabs = state.threads.map(t => (
-      {
-        title: t.title,
-        active: t.id === state.activeThreadId,
-        id: t.id,
-      }
-    ));
-    return (
-      <Tabs
-        tabs={tabs}
-        onClick={this.handleClick}
-      />
-    );
-  }
+  return {
+    tabs,
+  };
 }
+
+const mapDispatchToTabsProps = (dispatch) => {
+  return {
+    onClick: (id) => {
+      dispatch({
+        type: 'OPEN_THREAD',
+        id: id,
+      })
+    },
+  };
+}
+
+const ThreadTabs = connect(
+  mapStateToTabsProps,
+  mapDispatchToTabsProps
+)(Tabs);
 
 class ThreadDisplay extends React.Component {
   componentDidMount = () => {
@@ -248,4 +249,10 @@ const MessageList = (props) => (
   </div>
 );
 
-export default App;
+const WrappedApp = () => (
+  <Provider store={store}>
+    <App />
+  </Provider>
+);
+
+export default WrappedApp;
